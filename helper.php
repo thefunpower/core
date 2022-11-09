30ccd23a43efce24382f6014386c9c0b3077704e
sunkang <13127955213@qq.com>
2022-11-09 14:03:59 +0800

    jquery_checkbox_get_active

--- helper.php
+++ helper.php
@@ -1711,5 +1711,38 @@ function zip_create($local_zip_file,$files = []){
     return str_replace(PATH,'',$local_zip_file);
 }
 
-include __DIR__ . '/third/cjavascript.php';
-include __DIR__ . '/third/vue.php'; 
+/**
+* 获取checkbox选中数组 
+* jquery_checkbox_get_active('search','search')
+* <input type="checkbox" name="search" :value="k">
+*/
+function jquery_checkbox_get_active($var,$name){
+    return "let ".$var." = [];
+      \$('input[name=".$var."]').each(function(){
+        if(\$(this).prop('checked')){
+          ".$var.".push(\$(this).val());
+        }
+      });\n";
+}
+/**
+* checkbox全选|全不选
+*/
+function jquery_checkbox_select_all($name,$support_reversal = true){
+    $var = "var_jquery_checkbox_select_all".mt_rand(0,99999);
+    return " 
+      let ".$var." = false;
+      \$('input[name=".$name."]').each(function(){
+        if(\$(this).prop('checked') === true){
+            ".$var." = true;
+        }
+        if(".$var." === true){
+            \$(this).prop('checked',false);
+        }else{
+            \$(this).prop('checked',true);
+        }
+        
+      });\n";
+}
+
+include __DIR__ . '/yii1-javascript-encode.php';
+include __DIR__ . '/inc/vue.php'; 
