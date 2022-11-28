eb31d4cd23e10988ee9b920f87cb7559e740a809
sunkang <13127955213@qq.com>
2022-11-28 15:38:33 +0800

    Update Mail.php

--- lib/Mail.php
+++ lib/Mail.php
@@ -15,24 +15,26 @@ use Symfony\Component\Mailer\Mailer;
 use Symfony\Component\Mime\Email;
 
 /**
- * 需要在config.ini.php配置
- * 
-//邮件发送 
-$config['mail_from'] = "s19921272737@163.com";
-$config['mail_dsn']  = "smtp://".$config['mail_from'].":YCEHKDTNJUZXXYNL@smtp.163.com:25";
-
+ * 发送邮件
  * composer require symfony/mailer
  * https://symfony.com/components/Mailer
  */
 class Mail
 {
 	public static $mailer;
+	public static $mail_from;
 	//smtp://user:pass@smtp.example.com:25
 	public static function init($dsn = '')
 	{
 		global $config;
 		if (!$dsn) {
 			$dsn = $config['mail_dsn'];
+			$mail_from = get_config('mail_from');
+			$pwd = get_config('mail_pwd');
+			$mail_smtp = get_config('mail_smtp');
+			$mail_port = get_config('mail_port');
+			$dsn = "smtp://".$mail_from.":".$pwd."@".$mail_smtp.":".$mail_port; 
+			self::$mail_from = $mail_from;
 		}
 		ini_set("default_socket_timeout", 3);
 		$transport = Transport::fromDsn($dsn);
@@ -42,22 +44,19 @@ class Mail
 	/**
 	 *  发送邮件
    \lib\Mail::send([
-    'from'   => 's19921272737@163.com',
+    'from'   => 's7@163.com',
     'to'     => $data['email'],
-    'subject'=> "开通开票软件成功提醒", 
+    'subject'=> "开通成功提醒", 
     'html'   => "<p>登录网址为</p>"
-   ]);
-   更多参数如下：
-	from
-	to 
-	cc
-	bcc
-	replyTo
-	 */
+   ]); 
+   */
 	public static function send($config)
-	{
+	{  
 		if (!self::$mailer) {
 			self::init();
+		}
+		if(!$config['from']){
+			$config['from'] = self::$mail_from;
 		}
 		$email = (new Email());
 		foreach ($config as $k => $v) {
