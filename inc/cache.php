39e3ad3bab3b1b8b4663f84e94b60965ece1d509
sunkang <13127955213@qq.com>
2022-12-14 18:31:16 +0800

    v1.0.3准备工作

--- inc/cache.php
+++ inc/cache.php
@@ -2,7 +2,9 @@
 
 /*
     Copyright (c) 2021-2031, All rights reserved.
-    This is NOT a freeware, use is subject to license terms  
+    This is NOT a freeware, use is subject to license terms 
+    Connect Email: sunkangchina@163.com 
+    Code Vesion: v1.0
 */
 
 /*
