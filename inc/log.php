9a9349ebc2cf3c7fe723cc96f03aa46634c22cb9
sunkang <13127955213@qq.com>
2023-02-27 15:39:58 +0800

    log使用"monolog/monolog": "^2.9"

--- inc/log.php
+++ inc/log.php
@@ -7,117 +7,51 @@
     Code Vesion: v1.0.x
 */
 /**
- * 日志
- * @license read license.txt
- * @author sun <sunkangchina@163.com>
- * @copyright (c) 2021 
- */
-/**  
-write_log_info('一般');
-write_log_warn('警告');
-write_log_error('错误');
-write_log_error(['data'=>['a'=>1]]); 
- */
-
-
-/////////////////////////////////////////////////////////////
-// 生成日志
-/////////////////////////////////////////////////////////////
-
-use think\facade\Log;
-
-$log_from = "web";
-if (is_cli()) {
-    $log_from = 'cli';
-}
-$log_path = PATH . 'data/log/' . $log_from;
-Log::init([
-    'default'   =>  'file',
-    'channels'  =>  [
-        'file'  =>  [
-            'type'  =>  'file',
-            'path'  =>  $log_path,
-            'time_format'   =>    'Y-m-d H:i:s',
-        ],
-    ],
-]);
-
+ * 写日志
+ * log_info(string)
+ * 依赖 https://github.com/Seldaek/monolog
+*/
 /**
- * 写一般日志
- */
-function write_log_info($msg)
-{
-    write_log($msg, 'info');
+* 写日志
+*/
+function log_info($str,$name='app',$type='warning'){  
+    if(is_array($str)){
+        $str = json_encode($str,JSON_UNESCAPED_UNICODE);
+    }
+    log_init($type,$name)->$type($str,['ip'=>get_ip()]);
+}
+/**
+* 记录错误日志
+*/
+function log_error($str,$name='app'){  
+    log_info($str,$name,'error');
 }
 
 /**
- * 写警告日志
- */
-function write_log_warn($msg)
-{
-    write_log($msg, 'warning');
-}
-/**
- * 写错误日志
- */
-function write_log_error($msg)
-{
-    write_log($msg, 'error');
-}
-
-/**
- * 
- * 记录日志 
- * 日志的级别从低到高依次为： debug, info, notice, warning, error, critical, alert 
- * 以下级别会记录到数据库 
- * write_log(['txt'=>"错误了!!!",'trace'=>'登录成功'], 'alert');
- * write_log("错误了!!!", 'error');
- * write_log("错误了!!!", 'critical');
- * write_log("错误了!!!", 'alert'); 
- * 
- *          
-CREATE TABLE IF NOT EXISTS `log` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `level` varchar(100) NOT NULL COMMENT '',
-  `url` varchar(1000) NOT NULL COMMENT '',
-  `file` varchar(1000) NOT NULL COMMENT '',
-  `line` varchar(100) DEFAULT NULL COMMENT '',
-  `msg` json NOT NULL,
-  `trace` text NOT NULL COMMENT '', 
-  `created_at` datetime NULL COMMENT '',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4; 
-
- */ 
-function write_log($msg, $level = 'info')
-{ 
-    global $config; 
-    $level = strtolower($level); 
-    $trace = debug_backtrace(false);  
-    Log::write($trace, $level);  
-    $last = count($trace)-1;  
-    if ($config['log_db'] && in_array($level, ['warning', 'error', 'critical', 'alert'])) {
-        $arr = [];
-        $arr['url'] = urldecode($_SERVER['REQUEST_URI']);
-        $arr['level'] = $level;
-        $arr['file'] = str_replace("\\","/",$trace[$last]['file']);
-        $arr['line'] = $trace[$last]['line']; 
-        if(!is_array($msg)){
-            $msg = ['log'=>$msg];
-        }
-        $arr['msg']  = $msg;
-        $arr['trace'] = $trace;
-        $arr['created_at'] = now();  
-        $arr = db_allow("log",$arr);  
-        db_insert('log', $arr);   
-    } 
-    do_action("log", $trace); 
-} 
-
-/**
- * 关闭当前日志写入
- */
-function log_close()
-{
-    Log::close();
+* 初始化
+*/
+function log_init($WARNING='WARNING',$channel_name='app')
+{  
+    static $_log;
+    if($_log){
+        return $_log;
+    }
+    $log_from = "web";
+    if (is_cli()) {
+        $log_from = 'cli';
+    }
+    $log_path = PATH . 'data/log/' . $log_from; 
+    if(!is_dir($log_path)) mkdir($log_path,0777,true);
+    $log_file = $log_path.'/'.$channel_name.'.log'; 
+    $dateFormat = "Y-m-d H:i:s"; 
+    $output = "%datetime% > %level_name% > %message% %context% %extra%\n";
+    do_action("monolog.output",$output);
+    $_log = new Monolog\Logger($channel_name);
+    $stream = new Monolog\Handler\StreamHandler($log_file); 
+    $formatter = new Monolog\Formatter\LineFormatter($output, $dateFormat); 
+    $stream->setFormatter($formatter);
+    do_action("monolog",$_log);
+    do_action("monolog.stream",$stream);
+    $_log->pushHandler($stream);  
+    return $_log;
 }
